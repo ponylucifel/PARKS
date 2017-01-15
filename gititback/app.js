2d9c246ebda769f0e562b837cf216258966273c4
stytch16 <stanlec1@uci.edu>
2017-01-14 21:35:26 -0800

    started backend branch; worked on login/signup

--- gititback/app.js
+++ gititback/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/test');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
