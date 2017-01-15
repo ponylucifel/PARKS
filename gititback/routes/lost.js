8c0d2ae1cd828c8d073f44ff7ac886832db9f7ee
stytch16 <stanlec1@uci.edu>
2017-01-15 04:39:02 -0800

    worked on lost items

--- gititback/routes/lost.js
+++ gititback/routes/lost.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var ItemModel = require('../models/itemSchema')
+var itemModel = require('../models/itemSchema')
+var userModel = require('../models/userSchema')
+var moment = require('moment')
 
-router.post('/', function(req, res, next) {
-    //res.render('response', {title:''});
+router.get('/', function(req, res, next) {
+	    console.log(req.body.username);
+	        res.render('lost', { title: 'Lost Item', username:req.body.username});
+});
+
+router.post('/', function(req, res, next){
+	reporteddate = moment(req.body.date, ["MM-DD-YYYY", "DD-MM-YYYY"]).unix();
+	var newItem = new itemModel ({
+		username: req.body.username,
+	    	email: req.body.email,
+	    	phone: req.body.phone,
+	    	date: reporteddate,
+	    	location: req.body.location,
+	    	text: req.body.text
+	});
+	newItem.save(function(err,data){
+		if(err){
+			console.log(err)
+		} 
+		else {
+			console.log('Saved! ', newItem)
+	    		res.render('notify', { message: 'Your request has been submitted succesfully.  You will receive a notification through your email once someone found your item.', username:req.body.username})
+		}
+	});
 });
 
 
